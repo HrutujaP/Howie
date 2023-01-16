30847f3458cee6727090236e4ac055932ee437b4
Hrutuja Patnekar <hrutujapatnekar22@gmail.com>
2023-01-16 15:01:46 +0530

    connected to databse

--- howie/lib/main.dart
+++ howie/lib/main.dart
@@ -4,9 +4,13 @@ import 'package:howie/constants.dart';
 import 'package:howie/screens/ChatScreen/chatscreen.dart';
 import 'package:howie/screens/HomeScreen/homescreen.dart';
 import 'package:howie/screens/LandingScreen/landingscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarBrightness: Brightness.light,
       statusBarColor: kprimarycolor1,
@@ -14,10 +18,22 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,7 +42,7 @@ class MyApp extends StatelessWidget {
       routes: {
         LandingScreen.id: (context) => const LandingScreen(),
         HomeScreen.id: (context) => const HomeScreen(),
-        ChatScreen.id: (context) =>  ChatScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
